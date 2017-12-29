b8d3205fee942c18adb9c9c45f6533302c4ddf86
FlorianVaissiere <florian.vaissiere@orange.fr>
2017-12-29 18:09:11 +0100

    Projet commande implémenté

--- labicb/server/icbserv.py
+++ labicb/server/icbserv.py
@@ -24,28 +24,66 @@ while True:
       socks.append(c)
       buf = msg.encode('utf-8')
       c.send(buf)
-	if t=="/w":
 
-	if t=="/m":
+    if t.startswith('/w'):
+        if len(t[1:]) > 2:
+            print('User Detected: {}'.format(t[2:]))
+        else:
+            print('General Localtion')
 
-	if t=="/g":
+	if t.startswith('/m'):
+		if len(t[1:]) > 2:
+            print('Username Detected: {}'.format(t[2:]))
+        else:
+        	print('Missing a Recipient')
+        	msg="[=Error=] Missing a Recipient"
+			buf = msg.encode('utf-8')
+      		c.send(buf)  
 
-	if t=="/name":
+	if t.startswith('/g'):
+		if len(t[1:]) > 2:
+            print('Group name Detected: {}'.format(t[2:]))
+        else:
+        	print('Missing a Group Name')
+        	msg="[=Error=] Missing a Group Name"
+			buf = msg.encode('utf-8')
+      		c.send(buf) 
 
-	if t=="/topic":
+	if t.startswith('/name'):
+		if len(t[1:]) > 2:
+            print('NickName Detected: {}'.format(t[2:]))
+        else:
+        	print('Missing a Nickname')
+        	msg="[=Error=] Missing a New Nickname"
+			buf = msg.encode('utf-8')
+      		c.send(buf)
 
-	if t=="/pass":
+	if t.startswith('/topic'):
+		if len(t[1:]) > 2:
+            print('Topic Detected: {}'.format(t[2:]))
+        else:
+        	print('Missing a Topic')
+        	msg="[=Error=] Missing a Topic"
+			buf = msg.encode('utf-8')
+      		c.send(buf)
 
-	if t=="/?":
+	if t.startswith('/pass'):
+		msg="[=Notify=] Server has passed moderation to %s \n" % (addr[0],)
+		buf = msg.encode('utf-8')
+      	c.send(buf)
+
+	if t.startswith('/?'):
 		msg="[=Help=] Server supports following commands:\n" + 
 			"[=Help=] beep boot g m name nobeep pass topic w \n"
 		buf = msg.encode('utf-8')
       	c.send(buf)
-    if t=="/q":
-    	msg="[=Sign-off] %s JustLeft C\n" % (addr[0],)
+
+    if t.startswith('/q'):
+    	msg="[=Sign-off=] %s JustLeft \n" % (addr[0],)
 		buf = msg.encode('utf-8')
       	c.send(buf)
       	socks.remove(t)
+
     else: # someone is speaking
       who=t.getpeername()[0]
       data=t.recv(1024)
@@ -53,7 +91,7 @@ while True:
         msg="%s: %s\n" % (who, data.strip())
       else: # connection closed
         socks.remove(t)
-        msg="[=Sign-off] Drop Connection %s!\n" % (who,)
+        msg="[=Sign-off=] Drop Connection %s!\n" % (who,)
       print msg
       for c in socks[1:]:
         c.send(msg)
